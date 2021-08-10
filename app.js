e47812fea10a2e2b8476d0217139ca0cbd3fe82f
Jesus <33742064+jesusmonzon1@users.noreply.github.com>
2021-08-10 16:25:35 -0700

    0.1

--- app.js
+++ app.js
@@ -15,9 +15,44 @@ currentTime();
 
 // Bookmarks
 
-let bookmarks = [
+let bookmarksList = [
   {
     name: "Bank",
     link: "https://www.wellsfargo.com/",
+  },
+  {
+    name: "Email",
+    link: "https://mail.google.com/mail/u/0/#inbox",
+  },
+  {
+    name: "News",
+    link: "https://www.wsj.com/",
+  },
+  {
+    name: "Notes",
+    link: "https://www.evernote.com/client/web?login=true#?hm=true&",
   }
 ]
+
+
+
+
+
+function bookmarks() {
+  for (const book of bookmarksList) {
+    let bookmarkText = document.createElement("div");
+    let bookmarkLink = document.createElement("a");
+
+    bookmarkText.append(document.createTextNode(book.name));
+    bookmarkLink.href = book.link;
+
+    bookmarkText.classList.add("bookmark");
+    bookmarkLink.classList.add("bookmark-link");
+
+    bookmarkLink.append(bookmarkText);
+  
+    document.getElementById("bookmarkHold").appendChild(bookmarkLink);
+  }
+};
+
+bookmarks();
