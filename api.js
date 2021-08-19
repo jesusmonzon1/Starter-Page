3f6a52ed812e395f974c9248f8d3ba3f27be93c3
Jesus <33742064+jesusmonzon1@users.noreply.github.com>
2021-08-19 12:45:47 -0700

    0.6

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
+// declaring api variables
+let weatherapi = config.WEATHER_API;
+let financeapi = config.FINANCE_API;
+
 // UI for weather & finance API
 class UI {
   constructor() {
@@ -8,7 +12,6 @@ class UI {
     // Finance selectors
     this.price = document.getElementById('bitcoin');
   }
-
   display(weather) {
     // Temp and split the decimal
     this.temp.textContent = weather.main.temp;
@@ -19,7 +22,11 @@ class UI {
     // Get wind
     this.wind.textContent = weather.wind.speed
     let windSplit = this.wind.textContent;
-    this.wind.textContent = `wind ${windSplit.split(".")[0]} mph`;
+    if (windSplit === "0") {
+      this.wind.textContent = "no wind";
+    } else {
+      this.wind.textContent = `wind ${windSplit.split(".")[0]} mph`;
+    }
   }
   displayNumbers(bitcoin) {
     // Get current exchange rate for USD to bitcoin
@@ -32,12 +39,11 @@ const ui = new UI();
 // Weather API
 function weather() {
   let city = "Yuma";
-  let apiKey = "";
+  let apiKey = weatherapi;
 
   async function getWeather() {
     let response = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`);
     let weather = (await response).json();
-    console.log(weather);
     return weather;
   }
   getWeather().then(weather => ui.display(weather));
@@ -46,7 +52,7 @@ weather();
 
 // Financial API
 function bitcoin() {
-  let apiKey = ""
+  let apiKey = financeapi;
 
   async function getBitcoin() {
     let response = fetch(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=${apiKey}`);
